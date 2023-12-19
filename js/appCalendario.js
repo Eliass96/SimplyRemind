8a5870b0d286a9e424ddb371b54b541afce30f88
Elias <eliasbg2000@gmail.com>
2023-12-20 00:57:39 +0100

    Calendario bastante avanzado.

--- js/appCalendario.js
+++ js/appCalendario.js
@@ -1,18 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      locale: 'es',
-      headerToolbar: {
-        left: 'prev',
-        center: 'title',
-        right: 'next'
-      }
-      /*headerToolbar: {
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    themeSystem: "bootstrap5",
+    initialView: "dayGridMonth",
+    locale: "es",
+    customButtons: {
+      miLogo: {
+        icon: "../img/calendario.png",
+        click: function () {
+        },
+      },
+      botonMasOpciones: {
+        icon: "../icon/bars.svg",
+        click: function () {
+          botonMasOpciones.click();
+        },
+      },
+      botonPrev: {
+        icon: "../icon/circle-arrow-left.svg",
+        click: function () {
+          calendar.prev();
+        },
+      },
+      botonNext: {
+        icon: "../icon/circle-arrow-right.svg",
+        click: function () {
+          calendar.next();
+        },
+      },
+    },
+    headerToolbar: {
+      left: "miLogo botonPrev",
+      center: "title",
+      right: "botonNext botonMasOpciones",
+    },
+    /*headerToolbar: {
         left: 'prev, next, today',
         center: 'title',
         right: 'dayGridMonth, timeGridWeek, listWeek'
       }*/
-    });
-    calendar.render();
   });
+  calendar.render();
+});

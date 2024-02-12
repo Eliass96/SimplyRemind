ea41aeb992f1cec7a7152aeaea53bfe60b4ffc44
Elias <eliasbg2000@gmail.com>
2024-02-12 20:16:55 +0100

    dialogo calendario a medias

--- public/js/appCalendario.js
+++ public/js/appCalendario.js
@@ -1,45 +1,144 @@
+let calendarEl = document.getElementById('calendar');
+let frm = document.getElementById('formulario');
+let eliminar = document.getElementById('btnEliminar');
+let myModal = new bootstrap.Modal(document.getElementById('myModal'));
+
 document.addEventListener("DOMContentLoaded", function () {
-  var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
-      themeSystem: "bootstrap5",
-      initialView: "dayGridMonth",
-      locale: "es",
-      selectable: true,
-      customButtons: {
-          miLogo: {
-              text: "Mi Logo", // Agrega el atributo 'text'
-              icon: "../img/calendario.png",
-              click: function () {
-                  // Lógica al hacer clic en el botón miLogo
-              },
-          },
-          addEvento: {
-              text: "", // Agrega el atributo 'text'
-              icon: "/public/icon/bars.svg",
-              click: function () {
-                  // Lógica al hacer clic en el botón but_calendario
-              },
-          },
-          botonPrev: {
-              icon: "../icon/circle-arrow-left.svg",
-              click: function () {
-                  calendar.prev();
-              },
-          },
-          botonNext: {
-              icon: "../icon/circle-arrow-right.svg",
-              click: function () {
-                  calendar.next();
-              },
-          },
+    themeSystem: "bootstrap5",
+    initialView: "dayGridMonth",
+    locale: "es",
+    selectable: true,
+    customButtons: {
+      miLogo: {
+        text: "Mi Logo", // Agrega el atributo 'text'
+        icon: "../img/calendario.png",
+        click: function () {
+          // Lógica al hacer clic en el botón miLogo
+        },
       },
-      headerToolbar: {
-          left: "miLogo botonPrev",
-          center: "title",
-          right: "botonNext addEvento",
+      addEvento: {
+        text: "", // Agrega el atributo 'text'
+        icon: "/public/icon/bars.svg",
+        click: function () {
+          // Lógica al hacer clic en el botón but_calendario
+        },
       },
-      firstDay: 1,
+      botonPrev: {
+        icon: "../icon/circle-arrow-left.svg",
+        click: function () {
+          calendar.prev();
+        },
+      },
+      botonNext: {
+        icon: "../icon/circle-arrow-right.svg",
+        click: function () {
+          calendar.next();
+        },
+      },
+    },
+    headerToolbar: {
+      left: "miLogo botonPrev",
+      center: "title",
+      right: "botonNext addEvento",
+    },
+    editable: true,
+    dateClick: function (info) {
+      frm.reset();
+      eliminar.classList.add("d-none");
+      document.getElementById("start").value = info.dateStr;
+      document.getElementById("id").value = "";
+      document.getElementById("btnAccion").textContent = "Registrar";
+      document.getElementById("titulo").textContent = "Registrar Evento";
+      myModal.show();
+    },
+    eventClick: function (info) {
+      document.getElementById("id").value = info.event.id;
+      document.getElementById("title").value = info.event.title;
+      document.getElementById("start").value = info.event.startStr;
+      document.getElementById("color").value = info.event.backgroundColor;
+      document.getElementById("btnAccion").textContent = "Modificar";
+      document.getElementById("titulo").textContent = "Actualizar Evento";
+      eliminar.classList.remove("d-none");
+      myModal.show();
+    },
+    eventDrop: function (info) {
+      const start = info.event.startStr;
+      const id = info.event.id;
+      const url = base_url + "Home/drag";
+      const http = new XMLHttpRequest();
+      const formDta = new FormData();
+      formDta.append("start", start);
+      formDta.append("id", id);
+      http.open("POST", url, true);
+      http.send(formDta);
+      http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          console.log(this.responseText);
+          const res = JSON.parse(this.responseText);
+          Swal.fire("Avisos?", res.msg, res.tipo);
+          if (res.estado) {
+            myModal.hide();
+            calendar.refetchEvents();
+          }
+        }
+      };
+    },
+    firstDay: 1,
   });
   calendar.render();
+  frm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const start = document.getElementById("start").value;
+    if (title == "" || start == "") {
+      Swal.fire("Aviso", "Todo los campos son obligatorios", "warning");
+    } else {
+      const url = base_url + "Home/registrar";
+      const http = new XMLHttpRequest();
+      http.open("POST", url, true);
+      http.send(new FormData(frm));
+      http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          console.log(this.responseText);
+          const res = JSON.parse(this.responseText);
+          Swal.fire("Avisos?", res.msg, res.tipo);
+          if (res.estado) {
+            myModal.hide();
+            calendar.refetchEvents();
+          }
+        }
+      };
+    }
+  });
+  eliminar.addEventListener("click", function () {
+    myModal.hide();
+    Swal.fire({
+      title: "Advertencia?",
+      text: "Esta seguro de eliminar!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const url =
+          base_url + "Home/eliminar/" + document.getElementById("id").value;
+        const http = new XMLHttpRequest();
+        http.open("GET", url, true);
+        http.send();
+        http.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            const res = JSON.parse(this.responseText);
+            Swal.fire("Avisos?", res.msg, res.tipo);
+            if (res.estado) {
+              calendar.refetchEvents();
+            }
+          }
+        };
+      }
+    });
+  });
 });
-
--- public/js/index.js
+++ public/js/index.js
@@ -46,16 +46,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-// Abre el diálogo al hacer clic en el botón con id "but_etiquetar_nota"
-document.getElementById("but_etiquetar_nota").addEventListener("click", function () {
-  const etiquetarNotaModal = document.getElementById("etiquetar_nota");
-  etiquetarNotaModal.classList.add("show");
-  etiquetarNotaModal.style.display = "block";
-  document.body.classList.add("modal-open");
-});
+  // Abre el diálogo al hacer clic en el botón con id "but_etiquetar_nota"
+  document
+    .getElementById("but_etiquetar_nota")
+    .addEventListener("click", function () {
+      const etiquetarNotaModal = document.getElementById("etiquetar_nota");
+      etiquetarNotaModal.classList.add("show");
+      etiquetarNotaModal.style.display = "block";
+      document.body.classList.add("modal-open");
+    });
 
-// Cierra el diálogo al hacer clic en el botón con id "aceptar_etiqueta"
-document.getElementById("aceptar_etiqueta").addEventListener("click", function () {
+  // Cierra el diálogo al hacer clic en el botón con id "aceptar_etiqueta"
+  /*document.getElementById("aceptar_etiqueta").addEventListener("click", function () {
   const inputValue = document.getElementById("inputText").value;
   console.log("Texto ingresado:", inputValue);
   inputValue.textContent = "";
@@ -64,8 +66,7 @@ document.getElementById("aceptar_etiqueta").addEventListener("click", function (
   etiquetarNotaModal.classList.remove("show");
   etiquetarNotaModal.style.display = "none";
   document.body.classList.remove("modal-open");
-});
-
+});*/
 });
 
 function addNota() {
